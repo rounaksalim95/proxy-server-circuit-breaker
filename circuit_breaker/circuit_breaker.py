ad2f25b11276a9ee8fdde37db6e18730d5be1d22
rounaksalim95 <rounaksalim@gmail.com>
2021-11-23 20:40:13 -0800

    Add some more state keeping for basic circuit breaker

--- circuit_breaker/circuit_breaker.py
+++ circuit_breaker/circuit_breaker.py
@@ -8,19 +8,32 @@
 
 from functools import update_wrapper
 
+STATE_CLOSED = "CLOSED"
+STATE_OPEN = "OPEN"
+STATE_HALF_OPEN = "HALF_OPEN"
+
 
 class CircuitBreaker:
-    def __init__(self, *args):
-        for arg in args:
-            print(arg)
-        self.num_calls = 0
+    def __init__(self):
+        self._num_calls = 0
+        self._state = STATE_CLOSED
 
     def __call__(self, func):
         update_wrapper(self, func)
 
         def wrapper(*args, **kwargs):
-            print("Going to call function")
-            func(*args, **kwargs)
-            print("Function called")
+            if self._state == STATE_CLOSED:
+                print("Going to call function")
+                self._num_calls += 1
+                try:
+                    func(*args, **kwargs)
+                except Exception as e:
+                    self._state = STATE_OPEN
+                    print("Excpetion while calling function: ", e)
+                print("Function called %s time/s" % self._num_calls)
+            elif self._state == STATE_OPEN:
+                print("Not calling function as circuit breaker is in open state")
+            elif self._state == STATE_HALF_OPEN:
+                print("Circuit breaker is in half open state")
 
         return wrapper
--- driver.py
+++ driver.py
@@ -1,12 +1,18 @@
+import time
+
 import requests
 
 from circuit_breaker.circuit_breaker import CircuitBreaker
 
 
-@CircuitBreaker("test", "wee")
+@CircuitBreaker()
 def call_api(url):
     response = requests.get(url)
     print(response.json())
 
 
+call_api("http://127.0.0.1:5000/test")
+call_api("http://127.0.0.1:5000/test")
+time.sleep(5)
+call_api("http://127.0.0.1:5000/test")
 call_api("http://127.0.0.1:5000/test")
