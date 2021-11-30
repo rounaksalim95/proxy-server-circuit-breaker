3fc474593be93f57056d266df3c556b2cbc64939
rohitkphilip <rohitkurian.philip@sjsu.edu>
2021-11-30 15:32:28 -0800

    implemented server and proxy

--- proxy_server/server.py
+++ proxy_server/server.py
@@ -1,15 +1,49 @@
-from flask import Flask, jsonify
+from flask import Flask, request, Response
+import requests
+
 
 app = Flask(__name__)
 
+@app.route('/')
+def _proxy(*args, **kwargs):
+    print(request.url)
+    resp = requests.request(
+        method=request.method,
+        url=request.url.replace(request.host_url, 'http://127.0.0.1:5000/'),
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False)
+    
+    print(resp)
 
-@app.route("/test", methods=["GET"])
-def test():
-    """
-    GET endpoint for testing
-    """
-    return jsonify({"message": "Hello World!"})
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()
+               if name.lower() not in excluded_headers]
+
+    response = Response(resp.content, resp.status_code, headers)
+    return response
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/test/')
+def _proxy3(*args, **kwargs):
+    print(request.url)
+    print(request.url.replace(request.host_url, 'http://127.0.0.1:5000/'))
+    resp = requests.request(
+        method=request.method,
+        url=request.url.replace(request.host_url, 'http://127.0.0.1:5000/'),
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False)
+        
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()
+               if name.lower() not in excluded_headers]
+
+    response = Response(resp.content, resp.status_code, headers)
+    return response
+
+
+if(__name__ == "__main__"):
+    app.run(host='127.0.0.1', port=3000, debug=True)
